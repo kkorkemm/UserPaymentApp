52a981edb00374993acd28e0502ff7b0db8398e6
Mekrok <73012254+kkorkemm@users.noreply.github.com>
2021-04-08 13:04:44 +0600

    word

--- PaymentsApp/AdminWindow.xaml.cs
+++ PaymentsApp/AdminWindow.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 
 // подключение пространства имен для работы с excel
 using Excel = Microsoft.Office.Interop.Excel;
+// подключения пространства имен для работы с word
+using Word = Microsoft.Office.Interop.Word;
 
 namespace PaymentsApp
 {
@@ -129,6 +130,117 @@ namespace PaymentsApp
 
                 application.Visible = true;
             }
+        }
+
+        private void BtnWord_Click(object sender, RoutedEventArgs e)
+        {
+            var users = UserPaymentsDBEntities.GetContext().User.ToList();
+            var categories = UserPaymentsDBEntities.GetContext().PaymentType.ToList();
+
+            // новый экземпляр ворда
+            var application = new Word.Application();
+            // новый документ
+            Word.Document document = application.Documents.Add();
+
+            foreach (var user in users)
+            {
+                // текст на документе состоит из параграфов
+                Word.Paragraph userParagraph = document.Paragraphs.Add();
+                // доступ к параграфам осуществляется через Range
+                Word.Range userRange = userParagraph.Range;
+
+                // заголовок страницы
+                userRange.Text = user.FIO;
+                userRange.Paragraphs.set_Style("Заголовок");
+
+                // новый параграф
+                userRange.InsertParagraphAfter();
+
+                // таблица платежей
+                Word.Paragraph tableParagraph = document.Paragraphs.Add();
+                Word.Range tableRange = tableParagraph.Range;
+                // в какой параграф, количество строк, количество столбцов
+                Word.Table paymentTable = document.Tables.Add(tableRange, categories.Count + 1, 2);
+                // внутренние и внешние границы таблицы
+                paymentTable.Borders.InsideLineStyle =
+                paymentTable.Borders.OutsideLineStyle =
+                Word.WdLineStyle.wdLineStyleSingle;
+                // выравнивание содержимое ячеек по центру вертикали
+                paymentTable.Range.Cells.VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+
+                Word.Range cellRange; // ячейка
+
+                // названия колонок
+                cellRange = paymentTable.Cell(1, 1).Range;
+                cellRange.Text = "Категория";
+                cellRange = paymentTable.Cell(1, 2).Range;
+                cellRange.Text = "Сумма расходов";
+                //cellRange = paymentTable.Cell(1, 3).Range;
+                //cellRange.Text = "иконка";
+                // форматирование названий (жирный текст и расположение по центру)
+                paymentTable.Rows[1].Range.Bold = 1;
+                paymentTable.Rows[1].Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                //// колонка с иконками
+                //for (int i = 0; i < categories.Count(); i++)
+                //{
+                //    var currentCategory = categories[i];
+
+                //    cellRange = paymentTable.Cell(i + 2, 1).Range;
+                //    Word.InlineShape icon = cellRange.InlineShapes.AddPicture(AppDomain.CurrentDomain.BaseDirectory + "" + currentCategory);
+                //}
+
+
+                // заполнение колонок
+                for (int i = 0; i < categories.Count(); i++)
+                {
+                    var currentCategory = categories[i];
+
+                    // i + 2 - нумерация начинается с нуля, но в ворде с 1, а первая строка занята заголовками
+                    cellRange = paymentTable.Cell(i + 2, 1).Range;
+                    cellRange.Text = currentCategory.PaymentName;
+
+                    cellRange = paymentTable.Cell(i + 2, 2).Range;
+                    cellRange.Text = user.Payment.Where(p => p.PaymentType.PaymentName == currentCategory.PaymentName).Sum(p => p.Count * p.Price).ToString("N2");
+                }
+
+
+                Payment maxPayment = user.Payment.OrderByDescending(p => p.Price * p.Count).FirstOrDefault();
+
+                if (maxPayment != null)
+                {
+                    Word.Paragraph maxParagraph = document.Paragraphs.Add();
+                    Word.Range maxRange = maxParagraph.Range;
+
+                    maxRange.Text = $"Самый дорогостоящий платеж: {maxPayment.PaymentName} - {maxPayment.Summ}, от {maxPayment.PaymentDate.ToString("dd.MM.yyyy HH:mm")}";
+
+                    maxParagraph.set_Style("Цитата");
+                    maxRange.Font.Color = Word.WdColor.wdColorDarkRed;
+                    maxRange.InsertParagraphAfter();
+                }
+
+                Payment minPayment = user.Payment.OrderBy(p => p.Price * p.Count).FirstOrDefault();
+
+                if (minPayment != null)
+                {
+                    Word.Paragraph minParagraph = document.Paragraphs.Add();
+                    Word.Range minRange = minParagraph.Range;
+
+                    minRange.Text = $"Самый дешевый платеж: {minPayment.PaymentName} - {minPayment.Summ}, от {minPayment.PaymentDate.ToString("dd:MM:yyyy HH:mm")}";
+
+                    minParagraph.set_Style("Цитата");
+                    minRange.Font.Color = Word.WdColor.wdColorDarkGreen;
+                    minRange.InsertParagraphAfter();
+                }
+
+                // добавление разрыва страницы всем пользователям кроме последнего
+                if (user != users.LastOrDefault())
+                {
+                    document.Words.Last.InsertBreak(Word.WdBreakType.wdPageBreak);
+                }
+            }
+
+            application.Visible = true;
         }
     }
 }
